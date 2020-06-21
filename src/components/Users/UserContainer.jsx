5ffa03d22a042c68babd04a113bbea4a6dea05f8
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2020-06-21 23:42:38 +0400

    learn 56

--- src/components/Users/UserContainer.jsx
+++ src/components/Users/UserContainer.jsx
@@ -6,41 +6,52 @@ import {
   setUsersAC,
   setCurrentPagrAC,
   setUsersTotalCountAC,
+  toggleIsFetchingAC,
 } from "../../Redux/usersReducer";
 import * as axios from "axios";
 import Users from "./Users";
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
+    this.props.toggleIsFetchingAC(true);
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
       )
       .then((response) => {
+        this.props.toggleIsFetchingAC(false);
         this.props.setUsers(response.data.items);
         this.props.setTotalUsersCount(response.data.totalCount);
       });
   }
   onPageChanched = (pagesNumber) => {
+    this.props.toggleIsFetchingAC(true);
     this.props.setCurrentPage(pagesNumber);
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${pagesNumber}&count=${this.props.pageSize}`
       )
       .then((response) => {
+        this.props.toggleIsFetchingAC(false);
         this.props.setUsers(response.data.items);
       });
   };
   render() {
-    return <Users 
-    totalUsersCount={this.props.totalUsersCount}
-    pageSize={this.props.pageSize}
-    currentPage={this.props.currentPage}
-    onPageChanched={this.onPageChanched}
-    users={this.props.users}
-    follow={this.props.follow}
-    unfollow={this.props.unfollow}
-    />;
+    return (
+      <>
+        {this.props.isFetching ? <Preloader/> : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          currentPage={this.props.currentPage}
+          onPageChanched={this.onPageChanched}
+          users={this.props.users}
+          follow={this.props.follow}
+          unfollow={this.props.unfollow}
+        />
+      </>
+    );
   }
 }
 
@@ -50,6 +61,7 @@ let mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching,
   };
 };
 let mapDispatchToProps = (dispatch) => {
@@ -66,8 +78,11 @@ let mapDispatchToProps = (dispatch) => {
     setCurrentPage: (pageNumber) => {
       dispatch(setCurrentPagrAC(pageNumber));
     },
-    setTotalUsersCount: (totalCount)=> {
+    setTotalUsersCount: (totalCount) => {
       dispatch(setUsersTotalCountAC(totalCount));
+    },
+    toggleIsFetchingAC: (isFetching) => {
+      dispatch(toggleIsFetchingAC(isFetching));
     },
   };
 };
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import preloader from "../../../assets/images/Preloader.svg";
+
+let Preloader = (props) => {
+  return <img src={preloader} alt="Preloader" />;
+};
+export default Preloader;
